726f1a215c238e41c59307b5265ac168eda461f6
Greg Dziemidowicz <greg@cogent.co>
2015-07-05 12:33:42 +1000

    animations

--- static/js/index.js
+++ static/js/index.js
@@ -1,124 +1,152 @@
-var newMarker = null;
-var targetMarker = null;
-var socket = io.connect('http://govhack2015.gregdmd.com');
+  var newMarker = null;
+  var targetMarker = null;
+  var socket = io.connect('http://govhack2015.gregdmd.com');
 
-var howIfeel = null;
+  var howIfeel = null;
 
-var peers = {};
+  var peers = {};
 
-var showLocationFeel = function(latlng, feeling) {
-  if (feeling == 'safe') {
-          addSafePoint(latlng);
-      };
-  if (feeling == 'unsafe') {
-      addDangerPoint(latlng);
+  var showLocationFeel = function(latlng, feeling) {
+    if (feeling == 'safe') {
+            addSafePoint(latlng);
+        };
+    if (feeling == 'unsafe') {
+        addDangerPoint(latlng);
+    };
+    if (feeling == 'emergency') {
+        addEmergencyPoint(latlng);
+    }
+  }
+
+  function makeid()
+  {
+      var text = "";
+      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+      for( var i=0; i < 5; i++ )
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+      return text;
   };
-  if (feeling == 'emergency') {
-      addEmergencyPoint(latlng);
-  }
-}
 
-window.onload = function() {
-  cartodb.createVis('map', 'https://gdziemidowicz.cartodb.com/api/v2/viz/2ebe7fe6-21d6-11e5-ac0c-0e853d047bba/viz.json')
-  .done(function(vis, layers) {
-    layers[1].setInteraction(true);
+  window.onload = function() {
+    cartodb.createVis('map', 'https://gdziemidowicz.cartodb.com/api/v2/viz/2ebe7fe6-21d6-11e5-ac0c-0e853d047bba/viz.json')
+    .done(function(vis, layers) {
+      layers[1].setInteraction(true);
 
-    var map = vis.getNativeMap();
-    targetMarker = new L.marker([0,0]).addTo(map);
+      var map = vis.getNativeMap();
+      targetMarker = new L.marker([0,0]).addTo(map);
 
-    layers[1].on('featureClick', function(e, latlng, pos, data, layerNumber) {
-      targetMarker.setLatLng(latlng);
-      showLocationFeel(latlng, howIfeel);
-      socket.emit('user_click', { pos: latlng, id: socket.id, feeling: howIfeel} );
-    });
+      layers[1].on('featureClick', function(e, latlng, pos, data, layerNumber) {
+        targetMarker.setLatLng(latlng);
+        showLocationFeel(latlng, howIfeel);
+        socket.emit('user_click', { pos: latlng, id: socket.id, feeling: howIfeel} );
+      });
 
-    socket.on('user_click', function(msg){
-      if (peers[msg.id]) {
-        peers[msg.id].setLatLng(msg.pos);
-      } else {
-        peers[msg.id] = new L.marker(msg.pos);
-        peers[msg.id].addTo(map);
+      socket.on('user_click', function(msg){
+        if (peers[msg.id]) {
+          peers[msg.id].setLatLng(msg.pos);
+        } else {
+          peers[msg.id] = new L.marker(msg.pos);
+          peers[msg.id].addTo(map);
+        }
+        showLocationFeel(msg.pos, msg.feeling);
+      });
+
+      function showPosition(position) {
+        var pos = [position.coords.latitude,position.coords.longitude];
+        var my_location = new L.marker(pos).addTo(map);
+
+        map.panTo(pos);
+        map.setZoom(17);
+        
+        socket.emit('user_click', 
+          { pos: pos, id: socket.id }  
+          );
       }
-      showLocationFeel(msg.pos, msg.feeling);
-    });
 
-    function showPosition(position) {
-      var pos = [position.coords.latitude,position.coords.longitude];
-      var my_location = new L.marker(pos).addTo(map);
+      window.feelingSafe = function(position) {
+        feelingClickHandler('safe', position, addSafePoint)
+      };
 
-      map.setZoom(13);
-      map.panTo(pos);
+      window.feelingUnsafe = function(position) {
+        feelingClickHandler('unsafe', position, addDangerPoint)
+      };
 
-      socket.emit('user_click', 
-        { pos: pos, id: socket.id }  
-        );
-    }
+      window.feelingInEmergency = function(position) {
+        feelingClickHandler('emergency', position, addEmergencyPoint)
+      };
 
-    window.feelingSafe = function(position) {
-      feelingClickHandler('safe', position, addSafePoint)
-    };
+      window.feelingClickHandler = function(feeling, position, fun) {
+        latlng = [position.coords.latitude,position.coords.longitude];
+        howIfeel = feeling;
+        fun(latlng);
+        socket.emit('user_click', { pos: latlng, id: socket.id, feeling: feeling} );
+      };
 
-    window.feelingUnsafe = function(position) {
-      feelingClickHandler('unsafe', position, addDangerPoint)
-    };
+      window.addSafePoint = function addSafePoint(position) {
+        var pointId = makeid();
+        var my_location = new L.circle(position, 70, {
+          color: 'green',
+          fillColor: 'green',
+          fillOpacity: 0.5,
+          className: pointId
+        }).addTo(map);
 
-    window.feelingInEmergency = function(position) {
-      feelingClickHandler('emergency', position, addEmergencyPoint)
-    };
+        blinkblink(pointId);
+      }
 
-    window.feelingClickHandler = function(feeling, position, fun) {
-      latlng = [position.coords.latitude,position.coords.longitude];
-      howIfeel = feeling;
-      fun(latlng);
-      socket.emit('user_click', { pos: latlng, id: socket.id, feeling: feeling} );
-    };
-
-    window.addSafePoint = function addSafePoint(position) {
-      var my_location = new L.circle(position, 200, {
-        color: 'green',
-        fillColor: 'green',
-        fillOpacity: 0.5
+      window.addDangerPoint = function addDangerPoint(position) {
+       var pointId = makeid();
+       var my_location = new L.circle(position, 70, {
+        color: 'yellow',
+        fillColor: 'yellow',
+        fillOpacity: 0.7,
+        className: pointId
       }).addTo(map);
-    }
 
-    window.addDangerPoint = function addDangerPoint(position) {
-     var my_location = new L.circle(position, 200, {
-      color: 'yellow',
-      fillColor: 'yellow',
-      fillOpacity: 0.7
-    }).addTo(map);
+       blinkblink(pointId);
+     }
+
+     window.blinkblink = function(pointId) {
+      $("." + pointId).animate({ opacity: 0 }, 500, function() {
+             $("." + pointId).animate({ opacity: 1 }, 500, function() {})
+     });
    }
 
-   window.addEmergencyPoint = function addEmergencyPoint(position) {
-    console.log("addEmergencyPoint", position)
-     var my_location = new L.circle(position, 300, {
-      color: 'red',
-      fillColor: '#f03',
-      fillOpacity: 0.7
-    }).addTo(map);
-   }
+     window.addEmergencyPoint = function addEmergencyPoint(position) {
+      var pointId = makeid();
+      console.log("addEmergencyPoint", position)
+       var my_location = new L.circle(position, 100, {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.7,
+        className: pointId
+      }).addTo(map);
 
-   var x = document.getElementById("demo");
+       blinkblink(pointId);
+    };
 
-   function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition, 
-        function(error){
-          alert(error.message);
-        }, {
-          enableHighAccuracy: true
-          ,timeout : 25000
-        });
-    } else {
-      x.innerHTML = "Geolocation is not supported by this browser.";
+     
+     function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, 
+          function(error){
+            alert(error.message);
+          }, {
+            enableHighAccuracy: true
+            ,timeout : 25000
+          });
+      } else {
+        
+      }
     }
+
+    getLocation();
+
+  });
+
+
+
   }
-
-  getLocation();
-
-});
-
-
-
-}
 
